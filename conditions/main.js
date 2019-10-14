8d2c0ca0b0d72dbd34cd870afbe55bf342358d39
Glenn Dixon <glenndixon19@gmail.com>
2019-10-14 12:28:17 -0500

    less than

--- conditions/main.js
+++ conditions/main.js
@@ -31,3 +31,15 @@ function testGreaterOrEqual(val) {
 }
 
 console.log(testGreaterThanOrEqual(20));
+
+function testLessThan(val) {
+    if (val < 25)
+        return "Under 25";
+
+    if (val < 55)
+        return "Under 55";
+
+    return "55 or Over";
+}
+
+console.log(testLessThan(56));
