1cfaa6f6342e9d1f91e6862eece7339cfae80e9e
Glenn Dixon <glenndixon19@gmail.com>
2019-10-14 12:34:11 -0500

    less than or equal

--- conditions/main.js
+++ conditions/main.js
@@ -43,3 +43,15 @@ function testLessThan(val) {
 }
 
 console.log(testLessThan(56));
+
+function testLessOrEqual(val) {
+    if (val <= 12)
+        return "Smaller Than or Equal to 12";
+
+    if (val <= 24)
+        return "Smaller Than or Equal to 24";
+
+    return "More Than 24";
+}
+
+console.log(testLessThanOrEqual(13));
