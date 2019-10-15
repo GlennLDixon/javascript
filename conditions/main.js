00a4088afedf61eb9ba77c2d5bad0ad48c5d7efe
Glenn Dixon <glenndixon19@gmail.com>
2019-10-15 13:06:35 -0500

    commented out code

--- conditions/main.js
+++ conditions/main.js
@@ -1,83 +1,83 @@
-function testStrictNotEqual(val) {
-    if (val !== 17) {
-        return "Not equal";
-    }
-    return "Equal";
-}
+// function testStrictNotEqual(val) {
+//     if (val !== 17) {
+//         return "Not equal";
+//     }
+//     return "Equal";
+// }
 
-// Change this value to test
-console.log(testStrictNotEqual(10));
+// // Change this value to test
+// console.log(testStrictNotEqual(10));
 
-function testGreaterThan(val) {
-    if (val > 100)
-        return "Over 100";
+// function testGreaterThan(val) {
+//     if (val > 100)
+//         return "Over 100";
 
-    if (val > 10)
-        return "Over 10";
+//     if (val > 10)
+//         return "Over 10";
 
-    return "10 or Under";
-}
+//     return "10 or Under";
+// }
 
-console.log(testGreaterThan(101));
+// console.log(testGreaterThan(101));
 
-function testGreaterOrEqual(val) {
-    if (val >= 20)
-        return "20 or Over";
+// function testGreaterOrEqual(val) {
+//     if (val >= 20)
+//         return "20 or Over";
 
-    if (val >= 10)
-        return "10 or Over";
+//     if (val >= 10)
+//         return "10 or Over";
 
-    return "Less than 10";
-}
+//     return "Less than 10";
+// }
 
-console.log(testGreaterThanOrEqual(20));
+// console.log(testGreaterThanOrEqual(20));
 
-function testLessThan(val) {
-    if (val < 25)
-        return "Under 25";
+// function testLessThan(val) {
+//     if (val < 25)
+//         return "Under 25";
 
-    if (val < 55)
-        return "Under 55";
+//     if (val < 55)
+//         return "Under 55";
 
-    return "55 or Over";
-}
+//     return "55 or Over";
+// }
 
-console.log(testLessThan(56));
+// console.log(testLessThan(56));
 
-function testLessOrEqual(val) {
-    if (val <= 12)
-        return "Smaller Than or Equal to 12";
+// function testLessOrEqual(val) {
+//     if (val <= 12)
+//         return "Smaller Than or Equal to 12";
 
-    if (val <= 24)
-        return "Smaller Than or Equal to 24";
+//     if (val <= 24)
+//         return "Smaller Than or Equal to 24";
 
-    return "More Than 24";
-}
+//     return "More Than 24";
+// }
 
-console.log(testLessThanOrEqual(13));
+// console.log(testLessThanOrEqual(13));
 
-function testLogicalAnd(val) {
-    // Only change code below this line
+// function testLogicalAnd(val) {
+//     // Only change code below this line
 
-    if (val <= 50 && val >= 25) {
-        return "Yes";
-    }
+//     if (val <= 50 && val >= 25) {
+//         return "Yes";
+//     }
 
-    // Only change code above this line
-    return "No";
-}
+//     // Only change code above this line
+//     return "No";
+// }
 
-// Change this value to test
-console.log(testLogicalAnd(10));
+// // Change this value to test
+// console.log(testLogicalAnd(10));
 
-function testLogicalOr(val) {
-    // Only change code below this line
-    if (val < 10 || val > 20) {
-        return "Outside";
-    }
+// function testLogicalOr(val) {
+//     // Only change code below this line
+//     if (val < 10 || val > 20) {
+//         return "Outside";
+//     }
 
-    // Only change code above this line
-    return "Inside";
-}
+//     // Only change code above this line
+//     return "Inside";
+// }
 
-console.log(testLogicalOr(5));
+// console.log(testLogicalOr(5));
