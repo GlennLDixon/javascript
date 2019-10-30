08b1aee4ce6b5b2b68fb57ee328fc5ec92bfc736
Glenn Dixon <glenndixon19@gmail.com>
2019-10-30 11:54:06 -0500

    accessing properties with variables

--- main.js
+++ main.js
@@ -344,6 +344,18 @@ var testObj = {
 var entreeValue = testObj["an entree"]; // Change this line
 var drinkValue = testObj["the drink"]; // Change this line
 
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16; // Change this Line
+var player = testObj[playerNumber]; // Change this Line
+
 function multiplyAll(arr) {
     var product = 1;
     // Only change code below this line
