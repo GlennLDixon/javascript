3a9a41d2367579b3dbd729009181215d02530741
Glenn Dixon <glenndixon19@gmail.com>
2019-11-02 23:26:08 -0500

    Accessing Object properties with bracket notation

--- main.js
+++ main.js
@@ -354,7 +354,27 @@ var testObj = {
 // Only change code below this line;
 
 var playerNumber = 16; // Change this Line
-var player = testObj[playerNumber]; // Change this Line
+var player = testObj[playerNumber]; // Change this Linevar testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"]; // Change this line
+var drinkValue = testObj["the drink"]; // Change this line
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"]; // Change this line
+var drinkValue = testObj["the drink"]; // Change this line
 
 function multiplyAll(arr) {
     var product = 1;
